fd12664a7a6db6bdd10e6bfe404816e17100890a
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 19:45:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,8 @@ def assign_rooms(attendees)
 end
 
 def printer
-  batch_badge_creator
-  assign_rooms
-
+  puts batch_badge_creator(attendees)
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end
